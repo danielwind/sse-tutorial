d4d4d21d39325dfa617fd2a4fe2e2ccef8f12793
Daniel Wind <daniel.wind.m@gmail.com>
2020-07-15 09:28:51 -0600

    React Code Comments

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,37 +3,74 @@ import config from './../config/config';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
+
 const App = () => {
 
+  //---------------------------------------------------
+  //state variables to hold our data:
+  // employees = list of employees
+  // listen = flag to avoid unnecessary re-renders/subs
+  //---------------------------------------------------
   const [employees, setEmployees] = useState([]);
   const [listen, setListen] = useState(false);
-
+  
+  //-----------------------------------
+  // hook to perform side effects
+  // when data has changed! (re-render)
+  //-----------------------------------
   useEffect(() => {
+
 
     if(!listen){
 
-        //use the EventSource API 
-        //https://html.spec.whatwg.org/multipage/server-sent-events.html#the-eventsource-interface
+        //----------------------------------------------------
+        // use the EventSource API to subscribe to our API events
+        // more info on this api can be found in this link:
+        // https://html.spec.whatwg.org/multipage/server-sent- 
+        // events.html#the-eventsource-interface
+        //----------------------------------------------------
         const sourceEvents = new EventSource(config.SUBSCRIBE_ENDPOINT);
 
-        
+
+        //----------------------------------------------------
+        // if there are events, we are listening to the default
+        // "message" event, but you can listen to custom events
+        // as well. Here, we just repopulate the employees.
+        //-----------------------------------------------------
         sourceEvents.onmessage = (sourceEvent) => {
-          console.log(sourceEvent.data)
+            console.log(sourceEvent.data)
             setEmployees(JSON.parse(sourceEvent.data));
         };
 
+        
+        //----------------------------------------
+        // turn on flag to avoid multiple renders 
+        // and subscriptions
+        //----------------------------------------
         setListen(true);
     }
     
   }, [employees]);
 
+  
+  //----------------------------------------------------
+  // Helper function for data structure manipulation. 
+  // We could have built this object from the server
+  // and just parse it here but this is for illustration
+  // purposes only...
+  //----------------------------------------------------
   const getEmployeesAsJSON = () => {
     return employees.map((name, index) => {
        return {name: name, id: index }
     });
   }
 
+ 
+  //---------------------------------------------
+  // Helper function to render a bootstrap table2
+  //---------------------------------------------
   const render = () => {
+
 
     const columns = [
         {
@@ -47,8 +84,10 @@ const App = () => {
     ];
 
 
+
     return <BootstrapTable keyField='id' data={ getEmployeesAsJSON() } columns={ columns } />
   }
+
 
   return (
    <div className="d-flex justify-content-center mt-5">
@@ -58,5 +97,6 @@ const App = () => {
    </div>
   );
 };
+
 
 export default App;
